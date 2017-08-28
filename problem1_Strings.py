223fbabbf6b3cf12fb9a4c4f72782b428335e037
Manisha Tripathy <ManishaTripathy@users.noreply.github.com>
2017-08-27 21:34:59 -0500

    solution to problem1

---
+++ problem1_Strings.py
@@ -0,0 +1,24 @@
+﻿
+# Remove duplicates from a string, do it in-place
+
+T=int(input())
+in_strings=[]
+def remdup(s):
+    result=''
+    for i in s:
+        if i not in result:
+            result= result + i
+        else:
+            continue
+    return result
+    
+for i in range(0,T):
+    in_strings.append(input())
+	
+output=[]
+for s in in_strings:
+    output.append(remdup(s))
+	
+for o in output:
+    print(o)
+    

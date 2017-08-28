edf2965b70a64a4f03ef12528c5ea9b612823fbf
Manisha Tripathy <ManishaTripathy@users.noreply.github.com>
2017-08-27 21:32:25 -0500

    linked list reverse for given size

---
+++ problem2_linkedlist.py
@@ -0,0 +1,93 @@
+# Python program to reverse a linked list in group of given size
+ '''
+ Given a linked list, write a function to reverse every k nodes (where k is an input to the function).If a linked list is given as 1->2->3->4->5->6->7->8->NULL and k = 3 then output will be 3->2->1->6->5->4->8->7->NULL.
+
+Input:
+In this problem, method takes two argument: the head of the linked list and int k. You should not read any input from stdin/console.
+The struct Node has a data part which stores the data and a next pointer which points to the next element of the linked list. 
+There are multiple test cases. For each test case, this method will be called individually.
+
+Output:
+Reverse the linked list in the group of given size and return the reference of starting node(head) of the reversed Linked list .
+
+Note: If you use "Test" or "Expected Output Button" use below example format
+Example:
+Input:
+1
+8
+1 2 2 4 5 6 7 8
+4
+
+Output:
+4 2 2 1 8 7 6 5
+'''
+# Node class 
+class Node:
+ 
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+ 
+class LinkedList:
+ 
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+ 
+    def reverse(head, k):
+        current = head 
+        next  = None
+        prev = None
+        count = 0
+         
+        # Reverse first k nodes of the linked list
+        while(current is not None and count < k):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+            count += 1
+ 
+        # next is now a pointer to (k+1)th node
+        # recursively call for the list starting
+        # from current . And make rest of the list as
+        # next of first node
+        if next is not None:
+            head.next = self.reverse(next, k)
+ 
+        # prev is new head of the input list
+        return prev
+ 
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+ 
+    # Utility function to print the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print temp.data,
+            temp = temp.next
+ 
+ 
+# Driver program
+llist = LinkedList()
+llist.push(9)
+llist.push(8)
+llist.push(7)
+llist.push(6)
+llist.push(5)
+llist.push(4)
+llist.push(3)
+llist.push(2)
+llist.push(1)
+ 
+print "Given linked list"
+llist.printList()
+llist.head = llist.reverse(llist.head, 3)
+ 
+print "\nReversed Linked list"
+llist.printList()
